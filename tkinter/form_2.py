bce0303a18e1ba758a9a99a973d122a14a8981c6
sayyidul30 <muhammadsayyidul7@gmail.com>
2026-07-04 13:28:04 +0800

    commit kedua

--- tkinter/form_2.py
+++ tkinter/form_2.py
@@ -2,6 +2,36 @@ import tkinter
 from tkinter import ttk
 from tkinter import messagebox
 
+def enter_data():
+    accepted = accept_var.get()
+
+    if accepted == "Accept":
+        #user info
+        firstname = first_name_label.get()
+        lastname = last_name_label.get()
+
+        if firstname and lastname:
+            title = title_combobox.get()
+            age = age_spinbox.get()
+            nationality = nationality_combobox.get()
+
+            # course info
+            registration_status = reg_status_var.get()
+            numcourses = numcourses_spinbox.get()
+            numsemesters = numsemesters_spinbox.get()
+
+            print("first name: ", firstname, "last name: ", lastname)
+            print("Title: ", title, "age: ", age, "nationality: ", nationality)
+            print("# Course: ", numcourses, "# Semesters: ", numsemesters)
+            print("registration status: ", registration_status)
+            print("--------------------------------------------")
+        else:
+            tkinter.messagebox.showwarning(title= "Error",
+            message="first name and last name are required")
+    else:
+        tkinter.messagebox.showwarning(title= "Error",
+        message="you have not accepted terms and conditions")
+
 window = tkinter.Tk()
 window.title("data entry form")
 frame = tkinter.Frame(window)
@@ -48,15 +78,15 @@ variable=reg_status_var, onvalue="Registered", offvalue="NOT Registered")
 registered_label.grid(row=0, column=0)
 registered_check.grid(row=1, column=0)
 
-numcourse_label = tkinter.Label(course_frame, text= "# Completed Courses")
-numcourse_spinbox = tkinter.Spinbox(course_frame, from_=0, to= 'infinity')
-numcourse_label.grid(row=0, column=1)
-numcourse_spinbox.grid(row=1, column=1)
+numcourses_label = tkinter.Label(course_frame, text= "# Completed Courses")
+numcourses_spinbox = tkinter.Spinbox(course_frame, from_=0, to= 'infinity')
+numcourses_label.grid(row=0, column=1)
+numcourses_spinbox.grid(row=1, column=1)
 
-numcourse_label = tkinter.Label(course_frame, text= "# Semester")
-numcourse_spinbox = tkinter.Spinbox(course_frame, from_=0, to= 'infinity')
-numcourse_label.grid(row=0, column=2)
-numcourse_spinbox.grid(row=1, column=2)
+numsemesters_label = tkinter.Label(course_frame, text= "# Semester")
+numsemesters_spinbox = tkinter.Spinbox(course_frame, from_=0, to= 'infinity')
+numsemesters_label.grid(row=0, column=2)
+numsemesters_spinbox.grid(row=1, column=2)
 
 for widget in user_info_frame.winfo_children():
     widget.grid_configure(padx=5, pady=10)
@@ -75,70 +105,3 @@ button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
 
 window.mainloop()
 
-
-# # punya Neal --------------------------------------
-# import tkinter
-# from tkinter import ttk
-# from tkinter import messagebox
-
-# window = tkinter.Tk()
-# window.title("date entry form")
-# frame = tkinter.Frame(window)
-# frame.pack()
-
-# # saving user input
-# user_into_frame = tkinter.LabelFrame(frame, text="User Information")
-# user_into_frame.grid(row=0, column=0, padx=10, pady=10)
-
-# first_name_label = tkinter.Label(user_into_frame, text="First Name")
-# first_name_label.grid(row=0, column=0)
-# last_name_label = tkinter.Label(user_into_frame, text="Last Name")
-# last_name_label.grid(row=0, column=1)
-
-# first_name_label = tkinter.Entry(user_into_frame)
-# last_name_label = tkinter.Entry(user_into_frame)
-# first_name_label.grid(row=1, column=0)
-# last_name_label.grid(row=1, column=1)
-
-
-# title_label = tkinter.Label(user_into_frame, text="Title")
-# title_combobox = ttk.Combobox(user_into_frame, values=["Apa", "Mr", "Dr", "Prof"])
-# title_label.grid(row=0, column=2)
-# title_combobox.grid(row=1, column=2)
-
-# age_label = tkinter.Label(user_into_frame, text="Age")
-# age_spinbox = tkinter.Spinbox(user_into_frame, from_=8, to=50)
-# age_label.grid(row=2, column=0)
-# age_spinbox.grid(row=3, column=0)
-
-# nationality_label = tkinter.Label(user_into_frame, text="Nationality")
-# nationality_combobox = ttk.Combobox(user_into_frame, values=["Africa", "Antartika", "Asia", "Europe", "North America", "Oceania", "South America"])
-# nationality_label.grid(row=2, column=1)
-# nationality_combobox.grid(row=3, column=1)
-# for widget in user_into_frame.winfo_children():
-#     widget.grid_configure(padx=10, pady=5)
-
-# #saving course info
-# course_frame = tkinter.LabelFrame(frame)
-# course_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
-# registered_label = tkinter.Label(course_frame, text="Registration status")
-# reg_status_var = tkinter.StringVar(value="NOT Registered")
-# registered_check = tkinter.Checkbutton(course_frame, text="Curently Register",
-# variable=reg_status_var, onvalue="Registered", offvalue="NOT Registered")  #variabel (ini salah)
-# registered_label.grid(row=0, column=0)
-# registered_check.grid(row=1, column=0)
-
-# numcourse_label = tkinter.Label(course_frame, text="# Completed Courses")
-# numcourse_spinbox = tkinter.Spinbox(course_frame, from_=0, to= 'infinity')
-# numcourse_label.grid(row=0, column=1)   #colum (ini salah)
-# numcourse_spinbox.grid(row=1, column=1)
-
-# numcourse_label = tkinter.Label(course_frame, text="# Semester")
-# numcourse_spinbox = tkinter.Spinbox(course_frame, from_=0, to= 'infinity')
-# numcourse_label.grid(row=0, column=2)
-# numcourse_spinbox.grid(row=1, column=2)
-
-# for widget in user_into_frame.winfo_children():
-#     widget.grid_configure(padx=5, pady=10)
-
-# window.mainloop()
